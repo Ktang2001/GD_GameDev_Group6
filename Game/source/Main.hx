ff19d161661224f3276cd14771bb151a843154fb
Kaleb Tangen <ktangen2001@gmail.com>
2024-09-21 00:47:11 -0400

    Here are the initial Comments For Main

--- Game/source/Main.hx
+++ Game/source/Main.hx
@@ -1,5 +1,11 @@
 package;
 
+
+/* Group 6 Project 1
+Group Members: Kaleb Tangen, 
+Name of the Game: 
+
+*/
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
@@ -11,3 +17,9 @@ class Main extends Sprite
 		addChild(new FlxGame(0, 0, PlayState));
 	}
 }
+
+
+/* Sources
+
+
+ */
