fba9d879e426377470e9158e32242d43b2cfb27a
Kaleb Tangen <ktangen2001@gmail.com>
2024-10-07 15:29:20 -0400

    Intial Branch Changes

--- Game/export/neko/haxe/ApplicationMain.hx
+++ Game/export/neko/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "72";
+		app.meta["build"] = "73";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Game";
 		app.meta["name"] = "Game";
